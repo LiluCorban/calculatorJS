3c03d78761340c8dce0007355c4b0566cc684a2b
Yuliia Hryhorieva <yu.hryhorieva@gmail.com>
2023-01-17 20:56:09 +0200

    calc project

---
+++ js/script.js
@@ -0,0 +1,80 @@
+let firstNumber = "";
+let secondNumber = "";
+let operand = "";
+let finish = false;
+const digit = ["0","1","2","3","4","5","6","7","8","9","."];
+const action = ["*","/","+","-"];
+const out = document.querySelector(".screen");
+function clearAll(){
+    firstNumber = "";
+    secondNumber = "";
+    operand = "";
+    finish = false;
+    out.textContent = 0;
+}
+document.querySelector("#c").onclick = clearAll;
+document.querySelector(".button-container").onclick = function(event) {
+    if(!event.target.classList.contains("btn")) return;
+    if(event.target.classList.contains("c")) return;
+        
+    
+
+        out.textContent = "";
+
+        const key = event.target.textContent;
+       
+        console.log(key);
+        if(digit.includes(key)){
+            if(secondNumber === "" && operand === ""){
+            firstNumber += key;
+            out.textContent = firstNumber;
+            }
+            else if(firstNumber !== "" && secondNumber !== "" && finish){
+                secondNumber = key;
+                finish = false;
+                out.textContent = secondNumber;
+
+            }
+            else{
+                secondNumber += key;
+                out.textContent = secondNumber;
+            }
+            return;
+
+        }
+        if(action.includes(key)){
+            operand = key;
+            out.textContent = operand;
+            return
+
+        }
+        if(key === "="){
+            switch (operand) {
+                case "+":
+                    firstNumber = (+firstNumber) + (+secondNumber);
+                    break;
+
+                case "-":
+                    firstNumber = firstNumber - secondNumber;
+                    break;
+
+                case "*":
+                    firstNumber = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    if(secondNumber === "0"){
+                        out.textContent = "error";
+                        firstNumber = "";
+                        secondNumber = "";
+                        operand = "";
+                        return;
+                    }
+                    firstNumber = firstNumber / secondNumber;
+                    break;
+                
+            }
+            finish = true;
+            out.textContent = firstNumber;
+        }
+
+    }
